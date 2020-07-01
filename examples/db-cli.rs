a18b42634090425c85ce3edd8afbf1a9f71c9b69
Jesse Laning <jesse@jlaning.com>
2020-06-30 22:33:55 -0500

    Update db-cli.rs

--- examples/db-cli.rs
+++ examples/db-cli.rs
@@ -5,18 +5,16 @@ fn main() {
   if args.len() < 2 {
     return;
   }
-  let check_len = |n| {
-    if args.len() <= n {
-      None
-    } else {
-      Some(&args[n + 1])
-    }
+  let get_arg = |arg: &str| {
+    args.iter().position(|s| s == arg).and_then(|n| {
+      if args.len() <= n {
+        None
+      } else {
+        Some(&args[n + 1])
+      }
+    })
   };
-  let dbname = args
-    .iter()
-    .position(|s| s == "-n")
-    .and_then(check_len)
-    .expect("You must specify a database name with -n <name>");
+  let dbname = get_arg("-n").expect("You must specify a database name with -n <name>");
   if let Some(_) = args.iter().find(|&s| *s == "-c") {
     Database::new(dbname).expect("Failed to create db");
     println!("Created db {}", dbname);
@@ -30,27 +28,15 @@ fn main() {
   }
   if let Some(_) = args.iter().find(|&s| *s == "-s") {
     let mut db = Database::read(dbname.as_str()).expect("Failed to read db");
-    let key = args
-      .iter()
-      .position(|s| s == "-k")
-      .and_then(check_len)
-      .expect("You must specify a key to set with -k <key>");
-    let value = args
-      .iter()
-      .position(|s| s == "-v")
-      .and_then(check_len)
-      .expect("You must specify a value to set with -v <value>");
+    let key = get_arg("-k").expect("You must specify a key to set with -k <key>");
+    let value = get_arg("-v").expect("You must specify a value to set with -v <value>");
     db.insert(key, value).expect("Failed to insert key into db");
     println!("Inserted \"{}\": \"{}\" into db {}", key, value, dbname);
     return;
   }
   if let Some(_) = args.iter().find(|&s| *s == "-g") {
     let db = Database::read(dbname.as_str()).expect("Failed to read db");
-    let key = args
-      .iter()
-      .position(|s| s == "-k")
-      .and_then(check_len)
-      .expect("You must specify a key to set with -k <key>");
+    let key = get_arg("-k").expect("You must specify a key to set with -k <key>");
     let value: String = db.get(key).expect("Failed to get value from key in db");
     println!(
       "Got value \"{}\" associated with key \"{}\" from db {}",
